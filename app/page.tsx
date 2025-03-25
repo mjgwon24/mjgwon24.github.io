c3a00b632717ab523e2d6704dbafd13ddc4763f4
mjgwon24 <alswlchlrh8@naver.com>
2025-03-25 17:00:22 +0900

    feat: main - 포트폴리오 영역 head ui 구현

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import {useEffect, useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [height, setHeight] = useState<number>(0);
@@ -36,7 +37,7 @@ export default function Home() {
                 priority
             />
 
-            <h1 className="text-3xl sm:text-4xl weight-700 text-center leading-snug">
+            <h1 className="text-3xl sm:text-4xl weight-700 text-center leading-snug select-none">
               개발을 사랑하는 개발자,<br/>
               <span className="text-blue-500">권민지</span>의 홈에 오신것을 환영합니다.
             </h1>
@@ -44,11 +45,11 @@ export default function Home() {
 
           <div className="flex flex-col gap-5 items-center">
             <div className="flex flex-col gap-2 sm:gap-3 items-center">
-              <h2 className="text-2xl sm:text-3xl weight-700 pr-1">
+              <h2 className="text-2xl sm:text-3xl weight-700 pr-1 select-none">
                 🚀 기술 스택
               </h2>
 
-              <p className="sm:text-xl weight-500 pl-3">이런 기술을 사용해 개발해왔습니다.</p>
+              <p className="sm:text-xl weight-500 pl-3 select-none">이런 기술을 사용해 개발해왔습니다.</p>
             </div>
 
             <div className="flex flex-col gap-4 items-center">
@@ -152,60 +153,84 @@ export default function Home() {
 
 
           <div className="flex flex-col gap-5 items-center">
-            <div className="flex flex-col gap-2 sm:gap-3 items-center">
-              <h2 className="text-2xl sm:text-3xl weight-700 pr-1">
+            <div className="flex flex-col gap-2 sm:gap-3 items-center mb-6">
+              <h2 className="text-2xl sm:text-3xl weight-700 pr-1 select-none">
                 ✨ 포트폴리오
               </h2>
-
-              <p className="sm:text-xl weight-500 pl-3">제작한 프로젝트들, 어떤게 있을까요?</p>
+              <p className="sm:text-xl weight-500 pl-3 select-none">제작한 프로젝트들, 어떤게 있을까요?</p>
+              <Link
+                  href="/portfolio"
+                  className="text-white text-[14px] sm:text-[16px] py-1.5 px-8 rounded-lg weight-600 text-center mt-2"
+                  style={{background: "linear-gradient(90deg, #4A96EC 4.14%, #237BE6 97.19%), #FFF"}}
+              >
+                전체보기
+              </Link>
             </div>
 
-            <div className="flex flex-col gap-4 items-center">
-            </div>
+            {/*<div className="flex flex-col gap-4 items-center">*/}
+            {/*  <div className="flex flex-col sm:flex-row gap-4 items-center">*/}
+            {/*    <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg bg-black-10p">*/}
+
+
+
+
+
+
+            {/*    </div>*/}
+
+            {/*    <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg pt-3 pb-5 px-4 bg-black-10p">*/}
+
+
+
+
+
+
+            {/*    </div>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
           </div>
 
 
+          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            <li className="mb-2 tracking-[-.01em]">
+              Get started by editing{" "}
+              <code
+                  className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                app/page.tsx
+              </code>
+              .
+            </li>
+            <li className="tracking-[-.01em]">
+              Save and see your changes instantly.
+            </li>
+          </ol>
 
-              <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                <li className="mb-2 tracking-[-.01em]">
-                  Get started by editing{" "}
-                  <code
-                      className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                    app/page.tsx
-                  </code>
-                  .
-                </li>
-                <li className="tracking-[-.01em]">
-                  Save and see your changes instantly.
-                </li>
-              </ol>
-
-              <div className="flex gap-4 items-center flex-col sm:flex-row">
-                <a
-                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                  <Image
-                      className="dark:invert"
-                      src="/vercel.svg"
-                      alt="Vercel logomark"
-                      width={20}
-                      height={20}
-                  />
-                  Deploy now
-                </a>
-                <a
-                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                  Read our docs
-                </a>
-              </div>
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              <Image
+                  className="dark:invert"
+                  src="/vercel.svg"
+                  alt="Vercel logomark"
+                  width={20}
+                  height={20}
+              />
+              Deploy now
+            </a>
+            <a
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Read our docs
+            </a>
+          </div>
         </main>
       </div>
-);
+  );
 }
