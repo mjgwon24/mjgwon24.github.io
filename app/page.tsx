5a688b5899bcf11dacf14ef9d9547e4eda5e1227
mjgwon24 <alswlchlrh8@naver.com>
2025-03-26 15:03:01 +0900

    feat: main - 포트폴리오 영역 ui 구현

--- app/page.tsx
+++ app/page.tsx
@@ -167,27 +167,316 @@ export default function Home() {
               </Link>
             </div>
 
-            {/*<div className="flex flex-col gap-4 items-center">*/}
-            {/*  <div className="flex flex-col sm:flex-row gap-4 items-center">*/}
-            {/*    <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg bg-black-10p">*/}
+            <div className="flex flex-col gap-4 ">
+              <div className="flex flex-col sm:flex-row gap-4 items-center">
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/secubox.png"
+                      alt="Secubox project"
+                      width={320}
+                      height={250}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">보안 교육 시뮬레이션 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">25.02~25.03</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">정보보안 SW 웹/앱 개발 공모전</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">차세대 산업 보안 시뮬레이터 웹 플랫폼, SECUBOX입니다. 드래그 앤 드롭을 통해 쉽게 커스텀 네트워크 환경을 구성할 수 있으며, 공격 및 방어 시뮬레이션을 실행해 직접 보안 테스트를 진행할 수 있습니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1 flex-wrap">
+                      <div className="flex flex-row sm:gap-1.5 gap-1">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Back</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Design</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Security</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Education</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/semi-erp.png"
+                      alt="semi erp project"
+                      width={320}
+                      height={190}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">동아리 행정관리 ERP 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">25.01~25.03</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">동국대학교</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">동국대학교 동아리 행정 전반을 관리해주는 SEMI ERP 서비스입니다. 각 동아리별 인원 관리 및 예산 관리, 일정 관리를 할 수 있습니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1">
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Back</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Planning</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Design</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">ERP</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Admin Page</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
 
+              <div className="flex flex-col sm:flex-row gap-4 items-center">
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/softcat.png"
+                      alt="softcat project"
+                      width={320}
+                      height={250}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">맞춤형 솔루션 판매 서비스 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">24.06~24.11</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">소프트캣 (Softcat)</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">맞춤형 소프트웨어를 신청하거나 자동화 소프트웨어를 구독할 수 있는 서비스입니다. 원하는 구독 개월수 별로 구매하여 해당 소프트웨어를 사용할 수 있고, 관리자 페이지도 제공합니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1 flex-wrap">
+                      <div className="flex flex-row sm:gap-1.5 gap-1">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Back</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">CI/CD</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Commerce</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Admin Page</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/gyeongju-night.png"
+                      alt="gyeongju night project"
+                      width={320}
+                      height={190}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">숙소 예약 서비스 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">24.11~24.12</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">2024 경주 지역문제 해결 해커톤</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">2024 경주 해커톤 최우수상 수상! 경주만의 독특한 매력을 활용하여, 한옥·캠핑장 예약, 신선 지역 재료 직배송, 태그 이벤트 등을 녹여낸 숙박업소 예약 서비스입니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1">
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Back</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Planning</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Design</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Commerce</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col sm:flex-row gap-4 items-center">
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/stack-four-cut.png"
+                      alt="stack four cut project"
+                      width={320}
+                      height={250}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">네컷사진 촬영 서비스 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">24.07~24.10</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">IT동아리 DEVELOPER</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">사진을 찍어 네컷 사진으로 병합해주는 추억 남기기 서비스입니다. 경주시와 협업하여 “2024 경주 지역문제 해결 해커톤”에서 실제로 서비스를 사용하였고, 많은 행사 참가자들의 추억을 만들어주었습니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1 flex-wrap">
+                      <div className="flex flex-row sm:gap-1.5 gap-1">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Back</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Planning</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Design</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Entertainment</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col w-60 sm:w-80 rounded-lg bg-black-10p">
+                  <Image
+                      src="/portfolio/developer.png"
+                      alt="developer project"
+                      width={320}
+                      height={190}
+                      className="w-full h-[140px] sm:h-[190px] object-cover object-center rounded-t-lg"
+                  />
+
+                  <div className="flex flex-col justify-between sm:h-[250px] sm:h-[240px] p-4">
+                    <div className="flex flex-col sm:gap-1 gap-0 pb-4 sm:pb-0">
+                      <div
+                          className="flex flex-col-reverse sm:flex-row justify-start items-start sm:justify-between sm:items-center gap-1 sm:gap-0">
+                        <p className="text-sm sm:text-[16px] weight-600">개발동아리 사이트 개발</p>
+                        <div className="border border-blue-500 border-solid rounded-sm px-1.5 py-0.5">
+                          <p className="text-xs weight-400 text-blue-500">24.07~24.12</p>
+                        </div>
+                      </div>
+                      <span className="text-xs sm:text-sm text-gray-400 weight-400">IT동아리 DEVELOPER</span>
+                      <span className="text-xs sm:text-sm weight-400 sm:p-0 pt-1">동국대학교 개발 동아리 DEVELOPER의 공식 사이트입니다. 동아리 성과, 활동 내용, 부원 현황 등의 동아리 관련 정보를 제공해줍니다.</span>
+                    </div>
+
+                    <div className="flex flex-col sm:gap-1.5 gap-1">
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Front</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Planning</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400 text-blue-500">Design</span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-row sm:gap-1.5 gap-1 flex-wrap">
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">WEB</span>
+                        </div>
+                        <div className="pb-0.5 px-2 bg-black-10p rounded-sm">
+                          <span className="text-xs sm:text-sm weight-400">Community</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
 
-
-
-            {/*    </div>*/}
-
-            {/*    <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg pt-3 pb-5 px-4 bg-black-10p">*/}
-
-
-
-
-
-
-            {/*    </div>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
+            </div>
           </div>
 
 
