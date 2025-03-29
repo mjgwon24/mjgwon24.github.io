41e2e3c9ceceef4b75b3a3728a4633aae0ceb806
mjgwon24 <alswlchlrh8@naver.com>
2025-03-30 01:28:09 +0900

    update: 세부 디자인 개선

--- components/home/StackCard.tsx
+++ components/home/StackCard.tsx
@@ -7,11 +7,11 @@ interface StackCardProps {
 
 const StackCard: React.FC<StackCardProps> = ({ title, items }) => {
     return (
-        <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg pt-3 pb-5 px-4 bg-black-10p">
+        <div className="flex flex-col gap-4 w-60 sm:w-72 rounded-lg pt-3 pb-5 px-4 bg-black-05p">
             <span className="weight-600 text-blue-500 sm:text-lg">{title}</span>
             <div className="flex flex-row gap-2 flex-wrap text-sm weight-400 sm:weight-500">
                 {items.map((item, index) => (
-                    <div key={index} className="flex flex-col gap-2 rounded-md px-3 py-1.5 bg-black-10p">
+                    <div key={index} className="flex flex-col gap-2 rounded-md px-3 py-1.5 bg-black-05p">
                         <span>{item}</span>
                     </div>
                 ))}
