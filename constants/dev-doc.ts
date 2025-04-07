b448bb6f0f342390d97a0431e53dec8e579b4d17
mjgwon24 <alswlchlrh8@naver.com>
2025-04-07 14:49:40 +0900

    update: stack snapshot 내용 추가

--- constants/dev-doc.ts
+++ constants/dev-doc.ts
@@ -1,7 +1,13 @@
 export interface DevDocContent {
     title: string;
     description: string;
-    content: string;
+    content?: string;
+    image?: {
+        src: string;
+        alt: string;
+        width: number;
+        height: number;
+    };
 }
 
 export interface ProjectDevDoc {
@@ -9,9 +15,11 @@ export interface ProjectDevDoc {
     subtitle: string;
     sections: {
         planning: DevDocContent;
+        wbs: DevDocContent;
         requirements: DevDocContent;
         architecture: DevDocContent;
-        process: DevDocContent;
+        process?: DevDocContent;
+        flowchart: DevDocContent;
         api: DevDocContent;
         problemSolving: DevDocContent;
         results: DevDocContent;
@@ -28,9 +36,22 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
                 title: '기획',
                 description: '프로젝트 기획',
                 content: `대부분의 행사장에서 사진 촬영 부스를 운영하지만, 참가자들은 촬영한 사진을 현장에서 즉시 받을 수 없는 경우가 많습니다. 또한, 사진을 출력해주는 장비는 비용이 높고, 유지보수가 어렵습니다.
-
-<br />
-이를 해결하기 위해 “현장에서 촬영 → 즉시 편집 → QR 코드로 다운로드” 하는 디지털 방식의 네컷사진 서비스를 기획했습다. 추가로, QR 코드 다운로드 후 뽑기 이벤트를 추가하여 사용자 참여를 유도하고, 행사 몰입도를 높이는 요소를 추가하였습니다.`
+<br /><br/>
+이를 해결하기 위해 “현장에서 촬영 → 즉시 편집 → QR 코드로 다운로드” 하는 디지털 방식의 네컷사진 서비스를 기획했습니다. 추가로, QR 코드 다운로드 후 뽑기 이벤트를 추가하여 사용자 참여를 유도하고, 행사 몰입도를 높이는 요소를 추가하였습니다.
+<br /><br />
+주요 타겟 사용자<br/>
+• 행사 참가자: 친구, 동료와 함께 즐길 수 있는 사진 촬영 및 다운로드 기능 제공<br/>
+• 행사 주최자: 참가자들의 경험을 향상시키고, 경품 이벤트를 통해 행사의 재미 요소 추가`
+            },
+            wbs: {
+                title: 'WBS',
+                description: '프로젝트 WBS',
+                image: {
+                    src: '/portfolio/stack-snapshot/wbs.png',
+                    alt: '스택네컷 WBS',
+                    width: 800,
+                    height: 600
+                }
             },
             requirements: {
                 title: '프로젝트 요구사항',
@@ -95,7 +116,7 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
 # 2. 추가 요구사항
 
 ## 2.1 성능 및 보안
-- 동시 사용자 100명 이상 접속 시에도 원활한 동작 보장
+- 동시 사용자 10명 이상 접속 시에도 원활한 동작 보장
 - QR 코드 다운로드 및 사진 조합 기능 3초 이내 처리
 - 개인정보 보호를 위한 24시간 후 데이터 자동 삭제
 
@@ -106,12 +127,29 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
             architecture: {
                 title: '아키텍처 및 기술 설계',
                 description: '시스템 설계 및 구조',
-                content: '서비스의 전체 구조와 기술 스택...'
+                content: `
+# 1. 아키텍처 개요
+본 프로젝트는 한정된 자원 내에서 최적의 성능을 발휘해야 했기 때문에, 무료 크레딧 활용이 가능한 \`Naver Cloud Server\`를 배포 환경으로 선택하였습니다.<br />
+이와 같은 맥락으로, 파일 스토리지 또한 별도로 사용하지 않았습니다(파일 스토리지를 추가로 도입할 경우 이벤트용 서비스의 시스템 복잡성이 증가할 수 있고, 추가적인 관리 비용이 발생할 가능성 존재). 따라서 배포 환경 내 서버 폴더에 직접 파일을 저장하였습니다.<br />
+또한 이미지 처리와 QR 코드 생성 같은 고성능 연산이 필요한 기능을 고려하여 비동기 처리 및 캐싱 기법을 적용했습니다.
+<br /><br />
+# 2. 아키텍처 구성 요소
+- 백엔드: \`Spring Boot\`(Java 17)
+- 프론트엔드: \`React\`, \`Tailwind CSS\`
+- QR 코드 생성: ZXing 라이브러리
+- 이미지 처리: \`OpenCV\` 
+- 배포 환경: \`Naver Cloud Server\`, \`Nginx\`, GitHub Actions(CI/CD)
+`
             },
-            process: {
-                title: '작업 프로세스',
-                description: '진행 과정 및 워크플로우',
-                content: '개발 과정 중 사용한 절차 및 방법론...'
+            flowchart: {
+                title: '서비스 흐름도',
+                description: '서비스 흐름도 및 아키텍처',
+                image: {
+                    src: '/portfolio/stack-snapshot/flowchart.png',
+                    alt: '스택네컷 플로우차트',
+                    width: 800,
+                    height: 600
+                }
             },
             api: {
                 title: 'API 문서',
