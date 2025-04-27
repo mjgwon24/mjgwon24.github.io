303e91b4775570aefe6c5c379260446be44138f5
mjgwon24 <alswlchlrh8@naver.com>
2025-04-27 17:42:29 +0900

    update: 스택네컷 세부내용 추가

--- constants/dev-doc.ts
+++ constants/dev-doc.ts
@@ -42,7 +42,7 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
                 description: '프로젝트 기획',
                 content: `대부분의 행사장에서 사진 촬영 부스를 운영하지만, 참가자들은 촬영한 사진을 현장에서 즉시 받을 수 없는 경우가 많습니다. 또한, 사진을 출력해주는 장비는 비용이 높고, 유지보수가 어렵습니다.
 <br /><br/>
-이를 해결하기 위해 **“현장에서 촬영 → 즉시 편집 → QR 코드로 다운로드”** 하는 디지털 방식의 **네컷사진 서비스**를 기획했습니다. 추가로, QR 코드 다운로드 후 뽑기 이벤트를 추가하여 사용자 참여를 유도하고, 행사 몰입도를 높이는 요소를 추가하였습니다.
+이를 해결하기 위해 **“현장에서 촬영 → 즉시 편집 → QR 코드로 다운로드”** 하는 디지털 방식의 **네컷사진 서비스**, 스택네컷을 기획했습니다. 추가로, QR 코드 다운로드 후 뽑기 이벤트를 추가하여 사용자 참여를 유도하고, 행사 몰입도를 높이는 요소를 추가하였습니다.
 <br /><br />
 [ 주요 타겟 사용자 ]<br/>
 • 행사 참가자: 친구, 동료와 함께 즐길 수 있는 사진 촬영 및 다운로드 기능 제공<br/>
@@ -134,26 +134,26 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
                 title: '아키텍처 및 기술 설계',
                 description: '시스템 설계 및 구조',
                 content: `
+<img src="/portfolio/stack-snapshot/architecture.png" alt="stack-snapshot architecture" />
+
 # 1. 아키텍처 개요
 본 프로젝트는 한정된 자원 내에서 최적의 성능을 발휘해야 했기 때문에, 무료 크레딧 활용이 가능한 \`Naver Cloud Server\`를 배포 환경으로 선택하였습니다.<br />
 이와 같은 맥락으로, 파일 스토리지 또한 별도로 사용하지 않았습니다(파일 스토리지를 추가로 도입할 경우 이벤트용 서비스의 시스템 복잡성이 증가할 수 있고, 추가적인 관리 비용이 발생할 가능성 존재). 따라서 배포 환경 내 서버 폴더에 직접 파일을 저장하였습니다.<br />
 <br />
-# 2. 아키텍처 구성 요소
-- 백엔드: \`Spring Boot\`(Java 17)
+# 2. 기술 스택 설계
+- 백엔드: \`Spring Boot\`(Java 17), JPA, \`MySQL\`
 - 프론트엔드: \`React\`, \`Tailwind CSS\`
 - QR 코드 생성: ZXing 라이브러리
 - 이미지 처리: \`OpenCV\` 
-- 배포 환경: \`Naver Cloud Server\`, \`Nginx\`, GitHub Actions(CI/CD)
+- 인프라: \`Naver Cloud Server\`, \`Nginx\`, GitHub Actions(CI/CD)
 `
             },
             flowchart: {
-                title: '서비스 흐름도',
+                title: '시퀀스 다이어그램',
                 description: '서비스 흐름도 및 아키텍처',
                 image: {
                     src: '/portfolio/stack-snapshot/flowchart.png',
-                    alt: '스택네컷 플로우차트',
-                    width: 800,
-                    height: 600
+                    alt: '스택네컷 시퀀스 다이어그램',
                 }
             },
             api: {
@@ -285,7 +285,7 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
                 title: '문제 해결',
                 description: '문제 해결 및 개선 사항',
                 content: `
-# 비동기 방식 도입으로 인한 동시 디렉토리 생성 문제
+# Race Condition - 비동기 방식 도입으로 인한 동시 디렉토리 생성 문제
 \`CompletableFuture.supplyAsync\`를 사용하여 여러 스레드가 동시에 동일한 디렉토리에 접근하는 상황이 있었습니다. 디렉토리가 존재하지 않을 경우, 각 스레드가 디렉토리를 생성하려고 시도하게 됩니다. 이 과정에서 두 개 이상의 스레드가 동시에 \`groupDirectory.exists()\`를 호출한 후, **의도치않게 여러번 디렉토리를 생성**(race condition)할 가능성이 있습니다. 이러한 경우, "디렉토리 생성 실패"나 "IOException" 오류가 발생할 수 있습니다.
 <br/><br/>
 스택네컷 프로젝트에서도 이러한 문제를 경험했으며, 이를 해결하기 위해 적절한 동기화 메커니즘을 구현했습니다. 클래스 수준의 \`synchronized\` 블록을 사용하여 **한 번에 하나의 스레드만 해당 블록에 진입**할 수 있도록 했습니다. 
@@ -301,7 +301,7 @@ export const projectDevDocs: { [key: string]: ProjectDevDoc } = {
 <br /><br />
 JMeter를 활용해 최적화 방법을 직접 검증하고 성능 개선 결과를 확인할 때 매우 즐겁고 뿌듯했습니다. 함께 열심히 작업해준 팀원들에게도 무척 고맙고, 즐거운 경험이었습니다.
 <br /><br />
-이번 프로젝트는 함께한 팀원들의 첫 프로젝트였던 만큼, 모든 팀원이 많은 것을 배우고 지식을 얻어갈 수 있었던 보람찬 경험이 된 것 같아 정말 기쁩니다.
+이번 프로젝트는 함께한 팀원들의 첫 프로젝트였던 만큼, 모든 팀원이 많은 것을 배우고 지식을 얻어갈 수 있도록 노력했습니다. 모두에게 보람찬 경험이 된 것 같아 정말 기쁩니다!
                 `
             }
         }
