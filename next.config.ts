12a0fec8c936f028970cbe25bfe71e17e23bc244
mjgwon24 <alswlchlrh8@naver.com>
2025-06-15 14:13:37 +0900

    update: next.config.ts 설정 추가 및 Posts 컴포넌트 리팩토링

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    basePath: isProd ? "" : "",
+    assetPrefix: isProd ? "/mjgwon24.github.io" : "",
+    output: "export",
+    images: {
+        unoptimized: true
+    }
 };
+
+
 
 export default nextConfig;
