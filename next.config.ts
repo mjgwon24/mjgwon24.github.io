74c94a3fef3afc5441214f4654f70462b4b6186c
mjgwon24 <alswlchlrh8@naver.com>
2025-06-15 15:57:16 +0900

    update: ProjectDetail component

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    basePath: isProd ? "" : "",
-    assetPrefix: isProd ? "/mjgwon24.github.io" : "",
-    output: "export",
     images: {
         unoptimized: true
     }
